d02156463e33adfde3103c6dd347a9bbf023f76c
Arpit <arpitsengar99@gmail.com>
2023-06-15 21:35:49 +0530

    reduced to 5 loc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random, requests, streamlit as st
-def news(keyword="technology"): return requests.get(f'https://newsapi.org/v2/everything?q={keyword}&apiKey={st.secrets["NEWS_API"]}&language=en&searchIn=title').json()['articles']
+import os, requests, streamlit as st
+def news(keyword="technology"): return requests.get(f'https://newsapi.org/v2/everything?q={keyword}&apiKey={os.getenv("NEWS_API")}&language=en&searchIn=title').json()['articles']
 st.set_page_config(page_title="Today in Tech", page_icon=":newspaper:")
-st.markdown("""<style> #MainMenu {visibility: hidden;} footer {visibility: hidden;} </style> <h1 style='text-align: center; color: red;'>🔥 Today's top 10 news in Tech 🔥</h1> <h6 style='text-align: center; color: grey;'>(built under 5 lines of code, <a href="https://github.com/arpy8/tech-news-website">Repository Link</a>)</h6> <hr>""", unsafe_allow_html=True)
-for i in range(10): st.markdown(f"""<h2>{random.choice(news())['title']}</h2>\n<br><h5>{random.choice(news())['description']}</h5>\n<br>Link : <a href="{random.choice(news())['url']}">{random.choice(news())['url']}</a>\n<br>Author : {random.choice(news())['author']}, &nbsp; <i>{random.choice(news())['publishedAt'][:10]}</i> <hr>""", unsafe_allow_html=True)
+st.markdown("""<style> #MainMenu {visibility: hidden;} footer {visibility: hidden;} </style> <h1 style='text-align: center; color: red;'>🔥 Today's top 10 news in Tech 🔥</h1> <h6 style='text-align: center; color: grey;'>(built under 5 loc, <a href="https://github.com/arpy8/tech-news-website">Repository Link</a>)</h6> <hr>""", unsafe_allow_html=True)
+for random_news in list(news()):st.markdown(f"""<h2>{random_news['title']}</h2><h5>{random_news['description']}</h5>Link : <a href="{random_news['url']}">{random_news['url'][:50]}...</a><br>Author : {random_news['author']}, &nbsp; <i>{random_news['publishedAt'][:10]}</i> <hr>""", unsafe_allow_html=True)
